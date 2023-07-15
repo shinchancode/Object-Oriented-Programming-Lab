080726f5d5eb9653a1742b1afd4356fdfb111c5e
Aarti Rathi <72682683+shinchancode@users.noreply.github.com>
2023-07-15 17:51:26 +0530

    Added statement

--- 4 Class Area/Class Area.java
+++ 4 Class Area/Class Area.java
@@ -1,10 +1,10 @@
 /*AARTI RATHI
-My website - https://aartirathi17.herokuapp.com/ */
+My website - https://shinchancode.github.io/3d-react-portfolio/ */
 
 /*Problem Statement : 
-Design a base class shape with two double type values and member functions to input the data and compute_area() for calculating area of shape. Derive two classes: triangle and rectangle. Make compute_area() as abstract function and redefine this function in the derived class to suit their requirements. Write a program that accepts dimensions of triangle/rectangle and display calculated area. Implement dynamic binding for given case study. */
+Design a base class shape with two double type values and member functions to input the data and compute_area() for calculating area of shape. Derive two classes: triangle and rectangle. Make compute_area() as abstract function and redefine this function in the derived class to suit their requirements. Write a program that accepts dimensions of triangle/rectangle and display calculated area. Implement dynamic binding for given case study.
 
-----------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------*/
 
 package oop_assign4; 
 import java.util.*; 
