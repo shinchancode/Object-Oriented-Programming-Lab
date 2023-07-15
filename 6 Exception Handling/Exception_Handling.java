2b98464ea70f4e6a1224ffb37fd1c97760dc4973
Aarti Rathi <72682683+shinchancode@users.noreply.github.com>
2023-07-15 17:52:53 +0530

    Added statement

--- 6 Exception Handling/Exception_Handling.java
+++ 6 Exception Handling/Exception_Handling.java
@@ -1,10 +1,10 @@
 /*AARTI RATHI
-My website - https://aartirathi17.herokuapp.com/ */
+My website - https://shinchancode.github.io/3d-react-portfolio/ */
 
 /*Problem Statement :
-Implement a program to handle Arithmetic exception, Array Index Out of Bounds. The user enters two numbers Num1 and Num2. The division of Num1 and Num2 is displayed. If Num1 and Num2 are not integers, the program would throw a Number Format Exception. If Num2 were zero, the program would throw an Arithmetic Exception. Display the exception. */
+Implement a program to handle Arithmetic exception, Array Index Out of Bounds. The user enters two numbers Num1 and Num2. The division of Num1 and Num2 is displayed. If Num1 and Num2 are not integers, the program would throw a Number Format Exception. If Num2 were zero, the program would throw an Arithmetic Exception. Display the exception.
 
-----------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------------------- */
 
 package assign6 
 import java.util.Scanner; 
