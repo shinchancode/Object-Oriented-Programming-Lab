2d1cdfeace256cdbe2a3d4fead2053ac775c9df2
Aarti Rathi <72682683+shinchancode@users.noreply.github.com>
2023-07-15 17:59:28 +0530

    Added statement

--- 2 Publication/Publication.java
+++ 2 Publication/Publication.java
@@ -3,9 +3,9 @@ My website - https://shinchancode.github.io/3d-react-portfolio/
 */
 
 /*Problem Statement : 
-Identify commonalities and differences between Publication, Book and Magazine classes. Title, Price, Copies are common instance variables and saleCopy is common method. The differences are, Bookclass has author and orderCopies(). Magazine Class has methods orderQty, Current issue, receiveissue().Write a program to find how many copies of the given books are ordered and display total sale of publication. */
+Identify commonalities and differences between Publication, Book and Magazine classes. Title, Price, Copies are common instance variables and saleCopy is common method. The differences are, Bookclass has author and orderCopies(). Magazine Class has methods orderQty, Current issue, receiveissue().Write a program to find how many copies of the given books are ordered and display total sale of publication. 
 
-----------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------*/
 
 package assign_2; 
 import java.util.Scanner; 
